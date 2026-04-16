a3ea67db2038f3fe4a4799a55bc524d892409dd5
AveryanovKirill <144337512+AveryanovKirill@users.noreply.github.com>
2026-04-16 09:21:58 +0300

    Create FileData.cs

---
+++ FileData.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAuth
+{
+    public class FileData
+    {
+        public FileData(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
